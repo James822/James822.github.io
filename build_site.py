f45c16164df14b7a7227303bdc0a01b1e3f32522
James Spratt <JamesSpratt.business@protonmail.com>
2024-10-01 01:44:05 -0400

    made major changes to the website, it is now generated by a python script that takes the content in the src/ directory, and then builds it into html pages into the docs/ directory. I also changed the repository settings so that github would build from the docs/ folder instead. The python script is a little janky, but I'll iron it out later. For now, I have a working static site generator with the script, as I can create posts and put them in the src/posts/ directory and the script will then build posts from each file in that directory.

--- build_site.py
+++ build_site.py
@@ -1,1 +1,118 @@
- 
+import os
+import shutil
+
+SRC_DIR = "src/"
+IMAGES_DIR = "docs/images/"
+# PAGES_DIR = "docs/pages/"
+POSTS_DIR = SRC_DIR + "posts/"
+
+STYLES_FILE = "docs/css/styles.css"
+TITLE_FILE = SRC_DIR + "title.html"
+PANEL_FILE = SRC_DIR + "panel.html"
+MAIN_PAGE_FILE = SRC_DIR + "main_page.html"
+
+if __name__ == "__main__":
+    print("OS: " + os.name)
+    print()
+    print("Building site...")
+
+    # checking that directories exist
+    if not os.path.isdir(SRC_DIR):
+        print("bad path")
+        exit()
+    if not os.path.isdir(IMAGES_DIR):
+        print("bad path")
+        exit()
+    # if not os.path.isdir(PAGES_DIR):
+    #     print("bad path")
+    #     exit()
+    if not os.path.isdir(POSTS_DIR):
+        print("bad path")
+        exit()
+
+    # checking that files exist
+    if not os.path.isfile(STYLES_FILE):
+        print("bad file")
+        exit()
+    if not os.path.isfile(TITLE_FILE):
+        print("bad file")
+        exit()
+    if not os.path.isfile(PANEL_FILE):
+        print("bad file")
+        exit()
+    if not os.path.isfile(MAIN_PAGE_FILE):
+        print("bad file")
+        exit()
+
+        
+    title_file = open(TITLE_FILE, "r")
+    panel_file = open(PANEL_FILE, "r")
+    main_page_file = open(MAIN_PAGE_FILE, "r")
+
+    title_contents = title_file.read()
+    panel_contents = panel_file.read()
+    main_page_contents = main_page_file.read()
+
+    title_file.close()
+    panel_file.close()
+    main_page_file.close()
+
+    
+    # delete contents of docs/pages/ directory so that we can rebuild them
+    shutil.rmtree("docs/pages/")
+    
+    # re-create docs/pages/
+    os.mkdir("docs/pages")
+
+    
+    # building site
+    
+    title_marker_index = main_page_contents.find("<!--$title.html-->") # special HTML comment to denote where the title_contents should be inserted in a page
+    panel_marker_index = main_page_contents.find("<!--$panel.html-->") # special HTML comment to denote where the panel_contents should be inserted in a page
+    if title_marker_index == -1:
+        print("title_marker issue")
+        exit()
+    if panel_marker_index == -1:
+        print("panel_marker issue")
+        exit()
+    if not (title_marker_index < panel_marker_index):
+        print("title_marker is NOT before panel_marker")
+        exit()
+
+    built_main_page_contents = main_page_contents[:title_marker_index] + title_contents + main_page_contents[title_marker_index:panel_marker_index] + panel_contents + main_page_contents[panel_marker_index:]
+
+    index_file = open("docs/pages/index.html", "w")
+    index_file.write(built_main_page_contents)
+    index_file.close()
+
+    for filename in os.scandir("src/posts"):
+        if filename.is_file():
+            post_file = open(filename.path, "r")
+            post_contents = post_file.read()
+            post_file.close()
+
+            title_marker_index = post_contents.find("<!--$title.html-->") # special HTML comment to denote where the title_contents should be inserted in a page
+            panel_marker_index = post_contents.find("<!--$panel.html-->") # special HTML comment to denote where the panel_contents should be inserted in a page
+            if title_marker_index == -1:
+                print("title_marker issue")
+                exit()
+            if panel_marker_index == -1:
+                print("panel_marker issue")
+                exit()
+            if not (title_marker_index < panel_marker_index):
+                print("title_marker is NOT before panel_marker")
+                exit()
+
+            built_post_contents = post_contents[:title_marker_index] + title_contents + post_contents[title_marker_index:panel_marker_index] + panel_contents + post_contents[panel_marker_index:]
+
+            docs_post_file = open("docs/pages/" + filename.name, "w")
+            docs_post_file.write(built_post_contents)
+            docs_post_file.close()
+            
+    
+
+
+
+    
+
+
